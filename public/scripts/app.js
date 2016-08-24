04fbb2d7e6eb467612fcbfb0537def07460abe76
Emily Lordahl <emilylordahl@Emilys-MacBook-Pro.local>
2016-08-24 11:12:49 -0400

    typed out what the jsx syntax compiles to

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,14 +1,32 @@
+// var CommentBox = React.createClass({
+//   render: function() {
+//     return (
+//       <div className="commentBox">
+//       Hello world! I am a CommentBox.
+//       </div>
+//     ); // Closes return, which provides a native HTML div
+//   } // Closes render function
+// }); // Closes React class
+//
+// ReactDOM.render(
+//   <CommentBox />, // The React class begins with an uppcerase letter
+//   document.getElementById('content')
+// );
+
 var CommentBox = React.createClass({
-  render: function() {
-    return (
-      <div className="commentBox">
-      Hello world! I am a CommentBox.
-      </div>
-    ); // Closes return, which provides a native HTML div
-  } // Closes render function
-}); // Closes React class
+    displayName: "CommentBox",
+    render: function() {
+        return (
+            React.createElement("div", {
+                    className: "commentBox"
+                },
+                "Hello world! I am a commentBox."
+            )
+        );
+    }
+});
 
 ReactDOM.render(
-  <CommentBox />, // The React class begins with an uppcerase letter
-  document.getElementById('content')
+    React.createElement(CommentBox, null),
+    document.getElementById("content")
 );
