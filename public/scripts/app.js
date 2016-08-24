edd5dade6737ec55fdce70369c865eaa8a480087
Emily Lordahl <emilylordahl@Emilys-MacBook-Pro.local>
2016-08-24 12:31:29 -0400

    cleaning up some comments

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,29 +6,22 @@ var CommentBox = React.createClass({
         <CommentList data={this.props.data} />
         <CommentForm />
       </div>
-    ); // Closes return, which provides a native HTML div
-  } // Closes render function
-}); // Closes React class
-
-// var CommentBox = React.createClass({
-//   displayName: "CommentBox",
-//     render: function() {
-//       return (
-//         React.createElement("div", { // These divs are not actual DOM nodes, but instatntiations of React div compoents, which are essentially pieces of data React knows how to handle.
-//           className: "commentBox"
-//         },
-//           "Hello world! I am a derp derp CommentBox."
-//       )
-//     );
-//   }
-// });
+    );
+  }
+});
 
 var CommentList = React.createClass({
   render: function() {
+    var commentNodes = this.props.data.map(function(comment) {
+        return (
+          <Comment author={comment.author} key={comment.id}>
+            {comment.text}
+          </Comment>
+        );
+    });
     return (
       <div className="commentList">
-        <Comment author="Pete Hunt">This is one comment</Comment>
-        <Comment author="Jordan Walke">This is *another* comment</Comment>
+        {commentNodes}
       </div>
     );
   }
@@ -68,13 +61,8 @@ var data = [
   {id: 2, author: "Jordan Walke", text: "This is *another* comment"}
 ];
 
-// ReactDOM.render( // This instantiates the root component (CommentBox), starts the framework and injects the markup into a raw DOM element, provided as the second argument.
-//   React.createElement(CommentBox, null),
-//   document.getElementById("content")
-// );
-
 
 ReactDOM.render(
-  <CommentBox data={data} />, // The React class begins with an uppcerase letter
+  <CommentBox url />,
   document.getElementById('content')
 );
